e0c01283a3cdfb0ecbf979538b61f659cb8e3c84
Anastasia S. <annihilcax@gmail.com>
2026-02-21 18:05:36 +0300

    add: layout

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
-import './assets/css/style.css'
-import { oneFunc } from './modules/one.js'
-import { twoFunc } from './modules/two.js'
+import {oneFunc} from './modules/one.js'
+import {twoFunc} from './modules/two.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    
-  </div>
-`
+
 oneFunc();
 twoFunc();
