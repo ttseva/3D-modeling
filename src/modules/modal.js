250e6be46b6b3abd5cbb618472b07df5d6e80eff
Anastasia S. <annihilcax@gmail.com>
2026-02-21 21:15:53 +0300

    add: animation

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,11 +1,31 @@
-const modal= () => {
+const modal = () => {
   const modal = document.querySelector('.popup');
   const buttons = document.querySelectorAll('.popup-btn');
   const closeBtn = modal.querySelector('.popup-close');
 
+  const animationModal = () => {
+    let position = 100;
+    modal.style.display = 'block';
+    modal.style.left = '50%';
+
+    const slideUp = () => {
+      position -= 5;
+      modal.style.transform = `translateX(-50%) translateY(${position}%)`;
+
+      if (position > 0) {
+        requestAnimationFrame(slideUp);
+      } else {
+        modal.style.transform = 'translateX(-50%) translateY(0)';
+      }
+    };
+    requestAnimationFrame(slideUp);
+  };
+
   buttons.forEach(button => {
     button.addEventListener('click', (e) => {
-      modal.style.display = 'block';
+      if (!(window.innerWidth < 768)) {
+        animationModal();
+      }
     })
   })
 
