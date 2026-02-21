92daf979794cb97b57aaf39cbf3bc2f0351a4c2c
Anastasia S. <annihilcax@gmail.com>
2026-02-21 19:45:54 +0300

    fix: timer

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -4,6 +4,10 @@ const timer = (deadline) => {
   const timerMinutes = document.getElementById('timer-minutes');
   const timerSeconds = document.getElementById('timer-seconds');
 
+  const addZero = (num) => {
+    return num < 10 ? `0${num}` : num;
+  }
+
   const getTimeRemaining = () => {
     let dateStop = new Date(deadline).getTime();
     let dateNow = new Date().getTime();
@@ -12,6 +16,10 @@ const timer = (deadline) => {
     let hours = Math.floor((timeRemaining / 60 / 60) % 24);
     let minutes = Math.floor((timeRemaining / 60) % 60);
     let seconds = Math.floor(timeRemaining % 60);
+
+    if (seconds < 0) {
+      days = hours = minutes = seconds = 0;
+    }
 
     return {
       timeRemaining,
@@ -24,10 +32,10 @@ const timer = (deadline) => {
 
   const updateClock = () => {
     let getTime = getTimeRemaining();
-    timerDays.textContent = getTime.days;
-    timerHours.textContent = getTime.hours;
-    timerMinutes.textContent = getTime.minutes;
-    timerSeconds.textContent = getTime.seconds;
+    timerDays.textContent = addZero(getTime.days);
+    timerHours.textContent = addZero(getTime.hours);
+    timerMinutes.textContent = addZero(getTime.minutes);
+    timerSeconds.textContent = addZero(getTime.seconds);
 
     if (getTime.timeRemaining <= 0) clearInterval(timerInterval);
   }
