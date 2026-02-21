c6d6cd602e741ab603ca15d9f1498be6b3c46a6d
Anastasia S. <annihilcax@gmail.com>
2026-02-21 22:08:26 +0300

    add: form restrictions

--- src/modules/validation.js
+++ src/modules/validation.js
@@ -1,8 +1,26 @@
 const validation = () => {
   const calcInputs = document.querySelectorAll('.calc-item');
+  const formInputs = document.querySelectorAll('.main-form input, #form input, .footer-form input');
+  console.log(formInputs);
+
+
   calcInputs.forEach(input => {
+    if (input.type === 'text') {
+      input.addEventListener('input', (e) => {
+        e.target.value = e.target.value.replace(/\D+/g, '');
+      })
+    }
+  });
+
+  formInputs.forEach(input => {
     input.addEventListener('input', (e) => {
-      e.target.value = e.target.value.replace(/\D+/g, '');
+      if (input.type === 'text') {
+        e.target.value = e.target.value.replace(/[^а-яА-ЯёЁ\-\s]/g, '');
+      } else if (input.type === 'email') {
+        e.target.value = e.target.value.replace(/[^a-zA-Z0-9@\-_.!~*']/g, '');
+      } else if (input.type === 'tel') {
+        e.target.value = e.target.value.replace(/[^0-9\(\)\-]/g, '');
+      }
     })
   })
 }
