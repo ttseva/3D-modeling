412bc401b59c0bc563793a3fe439231198869ca3
Anastasia S. <annihilcax@gmail.com>
2026-02-26 00:26:23 +0300

    edit: menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,20 +1,20 @@
 const menu = () => {
   const menu = document.querySelector('menu');
-  const menuBtn = document.querySelector('.menu');
 
   const handleMenu = () => {
     menu.classList.toggle('active-menu');
   }
 
-  menuBtn.addEventListener('click', handleMenu);
+  const toggleMenu = () => {
+    document.addEventListener('click', (e) => {
+      if (e.target.closest('.menu') || e.target.matches('menu a') ||
+        (menu.classList.contains('active-menu') && !e.target.closest('menu'))) {
+        handleMenu();
+      }
+    });
+  }
 
-  menu.addEventListener('click', (e) => {
-    if (e.target.classList.contains('close-btn')) {
-      handleMenu();
-    } else if (e.target.matches('a')) {
-      handleMenu();
-    }
-  })
+  toggleMenu();
 }
 
 export default menu;
