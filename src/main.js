f26fb01a8197f30c1db9ed8295b0b12a3f95607b
Anastasia S. <annihilcax@gmail.com>
2026-02-25 22:32:09 +0300

    add: tabs

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import timer from './modules/timer.js'
 import menu from './modules/menu.js'
 import modal from './modules/modal.js'
 import validation from './modules/validation.js'
+import tabs from './modules/tabs.js'
 
 timer('28 march 2026');
 menu();
 modal()
 validation();
+tabs();
 
