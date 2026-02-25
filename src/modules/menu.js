f57ca6348d682ac60cf544afc037b1a25b2901b3
Anastasia S. <annihilcax@gmail.com>
2026-02-25 23:32:39 +0300

    add: modal + menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,18 +1,20 @@
 const menu = () => {
-  const menuBtn = document.querySelector('.menu');
   const menu = document.querySelector('menu');
-  const closeBtn = document.querySelector('.close-btn');
-  const menuItems = document.querySelectorAll('ul>li>a');
+  const menuBtn = document.querySelector('.menu');
 
   const handleMenu = () => {
     menu.classList.toggle('active-menu');
   }
 
-  menuBtn.addEventListener('click', handleMenu)
-  closeBtn.addEventListener('click', handleMenu)
-  menuItems.forEach(menuItem => {
-    menuItem.addEventListener('click', handleMenu);
-  });
+  menuBtn.addEventListener('click', handleMenu);
+
+  menu.addEventListener('click', (e) => {
+    if (e.target.classList.contains('close-btn')) {
+      handleMenu();
+    } else if (e.target.matches('a')) {
+      handleMenu();
+    }
+  })
 }
 
 export default menu;
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,7 +1,6 @@
 const modal = () => {
   const modal = document.querySelector('.popup');
   const buttons = document.querySelectorAll('.popup-btn');
-  const closeBtn = modal.querySelector('.popup-close');
 
   const animationModal = () => {
     let position = 100;
@@ -29,8 +28,10 @@ const modal = () => {
     })
   })
 
-  closeBtn.addEventListener('click', (e) => {
-    modal.style.display = 'none';
+  modal.addEventListener('click', (e) => {
+    if (!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) {
+      modal.style.display = 'none';
+    }
   })
 }
 
